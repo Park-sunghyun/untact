48754ca8bcb3cc15c64d421c7eb265ab9fe21123
Park-sunghyun <popol91@naver.com>
2021-01-25 23:59:01 +0900

    리스트 출력

--- src/main/java/com/sbs/untact/controller/UsrHomeController.java
+++ src/main/java/com/sbs/untact/controller/UsrHomeController.java
@@ -1,7 +1,9 @@
 package com.sbs.untact.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,18 @@ import com.sbs.untact.dto.Article;
 
 @Controller
 public class UsrHomeController {
+	private int articlesLastId;
 	
 	private List<Article> articles;
 	
 	public UsrHomeController() {
+		
+		articlesLastId = 0;
+		
 		articles = new ArrayList<>();
 		
-		articles.add(new Article(1,"2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(2,"2020-12-12 12:12:12", "제목2", "내용2"));
+		articles.add(new Article(++articlesLastId,"2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId,"2020-12-12 12:12:12", "제목2", "내용2"));
 		
 	}
 	
@@ -35,6 +41,55 @@ public class UsrHomeController {
 	public List<Article> showList() {
 		
 		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
+		
+		articles.add(new Article(++articlesLastId,regDate, title, body));
+		
+		Map<String, Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공 하였습니다.");
+		rs.put("id", articlesLastId);
+		
+		return rs;
+	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		
+		boolean deleteArticleRs = deleteArticle(id);
+		
+		
+		Map<String, Object> rs = new HashMap<>();
+		
+		if(deleteArticleRs ) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공 하였습니다.");	
+		}else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당 게시물은 존재하지 않습니다.");	
+		}
+		
+		rs.put("id", id);
+		
+		return rs;
+		
+		
+	}
+
+
+	private boolean deleteArticle(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
