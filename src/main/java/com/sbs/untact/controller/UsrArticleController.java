3e32f9ffa85c60ccb3a3be993eb44736c18a8fff
Park-sunghyun <popol91@naver.com>
2021-01-26 00:09:53 +0900

    게시글 작성

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.sbs.untact.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,17 @@ public class UsrArticleController {
 	
 	private List<Article> articles;
 	
+	private int ArticleLastId;
+	
 	
 	public UsrArticleController() {
 		
+		ArticleLastId = 0;
+		
 		articles = new ArrayList<>();
 		
-		articles.add(new Article(1, "2021-01-25 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(2, "2021-01-25 12:12:12", "제목2", "내용2"));
+		articles.add(new Article(++ArticleLastId, "2021-01-25 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++ArticleLastId, "2021-01-25 12:12:12", "제목2", "내용2"));
 			
 	}
 	
@@ -37,6 +43,23 @@ public class UsrArticleController {
 	public List<Article> showList() {
 				
 		return articles; 
+		
+	}
+	
+	
+	@RequestMapping("/usr/board/doAdd")
+	@ResponseBody
+	public Map<String, Object> showdoAdd(String regDate, String title, String body) {
+				
+		articles.add(new Article(++ArticleLastId, regDate, title, body));
+		
+		Map<String, Object> rs = new HashMap<>();
+		
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공");
+		rs.put("id", ArticleLastId);
+		
+		return rs; 
 		
 	}
 	
