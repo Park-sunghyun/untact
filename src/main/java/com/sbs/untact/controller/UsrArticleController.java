129c63ed613e65c63aa1c75f84eedae5239eac19
Park-sunghyun <popol91@naver.com>
2021-02-02 00:44:09 +0900

    게시물 컨트롤러 에서 입력데이터 유효성 체크

--- src/main/java/com/sbs/untact/controller/UsrArticleController.java
+++ src/main/java/com/sbs/untact/controller/UsrArticleController.java
@@ -7,42 +7,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.untact.dto.Article;
 import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.service.ArticleService;
 import com.sbs.untact.util.Util;
 
 @Controller
 public class UsrArticleController {
-	
-	private int ArticlesLastId;
-	private List<Article> articles;
-	
-	public UsrArticleController() {
+	@Autowired
+	private ArticleService articleService; 
 		
-		ArticlesLastId = 0;
-		
-		articles = new ArrayList<>();
-		
-		articles.add(new Article(++ArticlesLastId, "2021-01-26 12:12:12","2021-01-26 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(++ArticlesLastId, "2021-01-26 12:12:12","2021-01-26 12:12:12", "제목2", "내용2"));
-		
-
-		
-	} 
-	
-	
-	
 
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
-
-
-		return articles.get(id - 1);
+		Article article = articleService.getArticle(id);
+		
+		return article;
 
 	}
 
@@ -50,24 +36,26 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> showList() {
 
-		return articles;
+		return articleService.getArticles();
 
 	}
 	
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
-		SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");		
-		Date time = new Date();		
-		String regDate = Util.getNowDateStr();
-		String updateDate = regDate;
+		
+		if( title == null) {
+			return new ResultData( "F-1", "title 을 입력해주세요.");
+		}
+		if( body == null) {
+			return new ResultData( "F-1", "body 를 입력해주세요.");
+		}
+		
+		ResultData rsData = articleService.add(title, body);
 		
 		
-		articles.add(new Article(++ArticlesLastId, regDate, updateDate, title, body));
 		
-	
-		
-		return new ResultData("S-1","성공","id", ArticlesLastId);
+		return rsData;
 
 	}
 
@@ -76,63 +64,50 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public ResultData doDelete(int id) {
+	public ResultData doDelete(Integer id) {
 		
-		boolean deleteArticleRs = deleteArticle(id);
+		if( id == null) {
+			return new ResultData( "F-1", "id 를 입력해주세요.");
+		}
 		
+		Article article = articleService.getArticle(id);
 		
-		Map<String, Object> rs = new HashMap<>();
-		
-		if (deleteArticleRs == false) {
+		if( article == null) {
 			return new ResultData( "F-1", "해당 게시물은 존재하지 않습니다.");
-
-		} else {
-			return new ResultData( "S-1",  "성공 하였습니다.", "id", id);
 		}
 		
+		return articleService.deleteArticle(id);
+			
 		
-	}
-
-
-	private boolean deleteArticle(int id) {
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				articles.remove(article);
-				return true;
-			}
-		}
-		return false;
 	}
 	
 	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData doModify(int id, String title, String body) {
-		Article selarticle = null;
+	public ResultData doModify(Integer id, String title, String body) {
 		
 		
-		
-		
-		for(Article article : articles) {
-			if( article.getId() == id) {
-				selarticle = article;
-				break;
-			}
+		if( id == null) {
+			return new ResultData( "F-1", "id 를 입력해주세요.");
 		}
 		
-//		Map<String, Object> rs = new HashMap<>();
+		if( title == null) {
+			return new ResultData( "F-1", "title 을 입력해주세요.");
+		}
+		if( body == null) {
+			return new ResultData( "F-1", "body 를 입력해주세요.");
+		}
 		
-		if(selarticle == null ) {
+		
+		Article article = articleService.getArticle(id);
+		
+		if(article == null ) {
 			return new ResultData("F-1", String.format("%d 번 게시물은 존재하지 않습니다.", id));
 		}
-		
-		selarticle.setUpdateDate(Util.getNowDateStr());
-		selarticle.setTitle(title);
-		selarticle.setBody(body);
-		
 
-		return new ResultData("S-1", String.format("%d 번 게시물이 수정되었습니다.", id),"id", id);
 		
+		
+		return articleService.modify(id, title, body);		
 	}
 	
 	
---
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -0,0 +1,79 @@
+package com.sbs.untact.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sbs.untact.dto.Article;
+import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.util.Util;
+
+@Service
+public class ArticleService {
+
+	private int ArticlesLastId;
+	private List<Article> articles;
+
+	public ArticleService() {
+		ArticlesLastId = 0;
+
+		articles = new ArrayList<>();
+
+		articles.add(new Article(++ArticlesLastId, "2021-01-26 12:12:12", "2021-01-26 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++ArticlesLastId, "2021-01-26 12:12:12", "2021-01-26 12:12:12", "제목2", "내용2"));
+	}
+
+	public Article getArticle(int id) {
+		
+		for( Article article : articles ) {
+			
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		
+		
+		return null;
+	}
+
+	public List<Article> getArticles() {
+		
+		return articles;
+	}
+
+	public ResultData add(String title, String body) {
+		int id = ++ArticlesLastId;
+		String regDate = Util.getNowDateStr();
+		String updateDate = regDate;
+		
+		
+		articles.add(new Article(id, regDate, updateDate, title, body));
+		
+	
+		return new ResultData( "S-1",  "성공 하였습니다.", "id", id);
+	}
+
+		
+		public ResultData deleteArticle(int id) {
+			for(Article article : articles) {
+				if(article.getId() == id) {
+					articles.remove(article);
+					return new ResultData( "S-1",  "삭제 하였습니다.", "id", id);
+				}
+			}
+			return new ResultData( "S-1",  "해당 게시물 은 존재하지 않습니다.", "id", id);
+		}
+
+		public ResultData modify(int id, String title, String body) {
+			Article article = getArticle(id);
+			
+			article.setTitle(title);
+			article.setBody(body);
+			article.setUpdateDate(Util.getNowDateStr());
+			
+			
+			return new ResultData( "S-1",  "게시물 을 수정하였습니다.", "id", id);
+		}
+
+}
