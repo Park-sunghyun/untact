0e52c6b3fb479a7a9c80f40f3f4b2dfdd8bef8b2
Park-sunghyun <popol91@naver.com>
2021-02-07 20:09:46 +0900

    회원가입시 로그인아이디 중복체크

---
+++ src/main/java/com/sbs/untact/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.sbs.untact.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sbs.untact.dao.MemberDao;
+import com.sbs.untact.dto.Member;
+import com.sbs.untact.dto.ResultData;
+import com.sbs.untact.util.Util;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberDao memberDao;
+
+	public ResultData join(Map<String, Object> param) {
+		memberDao.join(param);
+
+		int id = Util.getAsInt(param.get("id"), 0);
+
+		return new ResultData("S-1", String.format("%s님 환영합니다.", param.get("nickname")), "id", id);
+	}
+
+	public Member getMember(int id) {
+
+		return memberDao.getMember(id);
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+
+		return memberDao.getMemberByLoginId(loginId);
+	}
+
+}
--- src/main/java/com/sbs/untact/util/Util.java
+++ src/main/java/com/sbs/untact/util/Util.java
@@ -7,13 +7,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Util {
-	public static String getNowDateStr() {
-		SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");		
-		Date time = new Date();		
-		
-		return format1.format(time);
-	}
-	
+//	public static String getNowDateStr() {
+//		SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");		
+//		Date time = new Date();		
+//		
+//		return format1.format(time);
+//	}
+//	
 	public static Map<String, Object> mapOf(Object... args) {
 		if ( args.length % 2 != 0 ) {
 			throw new IllegalArgumentException("인자를 짝수개 입력해주세요.");
